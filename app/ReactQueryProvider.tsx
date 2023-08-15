bfb96ab98aee11f5c5421955449c17e947ef6a4b
nestor22 <nestor.ge222@gmail.com>
2023-08-15 12:51:02 -0600

    [ADD] react query and provider, additional to styles and configuration

---
+++ app/ReactQueryProvider.tsx
@@ -0,0 +1,14 @@
+'use client';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
+export const ReactQueryProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
