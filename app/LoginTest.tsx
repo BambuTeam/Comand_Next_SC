11d0bc10ca646d65568b019c9f783a612283106d
Bambu Team |  Luis Enrique Concuá <75746976+BambuTeam@users.noreply.github.com>
2023-08-24 21:47:19 -0600

    Pimp Login

--- app/LoginTest.tsx
+++ app/LoginTest.tsx
@@ -1,12 +1,35 @@
 'use client';
 import { signIn } from 'next-auth/react';
+import Waiter from '../public/Images/home/mesero.svg';
+import Image from 'next/image';
 
 const LoginTest = () => {
   const handleClick = () => {
     signIn('google');
   };
 
-  return <button onClick={handleClick}>Click To Loging</button>;
+  return (
+    <div className='flex'>
+      <div className='w-1/2'>
+        <Image
+          src={Waiter}
+          alt='Picture of the author'
+          width='500'
+          height='500'
+          className='bg-mint text-mint fill-current'
+        ></Image>
+      </div>
+
+      <div>
+        <button
+          onClick={handleClick}
+          className=' focus:ring-white-600 bg-zinc-900 p-8 hover:bg-zinc-700 '
+        >
+          Click To Loging
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default LoginTest;
