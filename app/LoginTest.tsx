ac3516771b782a89f629d8477969f15a97141dff
Bambu Team |  Luis Enrique Concuá <75746976+BambuTeam@users.noreply.github.com>
2023-09-09 18:24:23 -0600

    upd

--- app/LoginTest.tsx
+++ app/LoginTest.tsx
@@ -16,7 +16,7 @@ const LoginTest = () => {
         <Lottie className='Lottie_style' animationData={animation} />
       </div>
 
-      <div className=' '>
+      <div>
         <button
           onClick={handleClick}
           className=' h-24 w-48 rounded-lg bg-gradient-to-r from-teal-200 to-lime-200 px-5 py-2.5 text-center text-sm font-medium text-gray-900 hover:bg-gradient-to-l hover:from-teal-200 hover:to-lime-200 focus:outline-none focus:ring-4 focus:ring-lime-200 dark:focus:ring-teal-700'
