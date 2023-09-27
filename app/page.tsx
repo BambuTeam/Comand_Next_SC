c01275359c9aeafb887f607b85eca1eebdb62ff8
nestor22 <nestor.ge222@gmail.com>
2023-09-26 19:08:44 -0600

    update the page and login validation in the main route

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 'use client';
+import React from 'react';
+import { signOut, useSession } from 'next-auth/react';
 
 export default function Home() {
+  const session = useSession();
   return (
     <main className='z-10 flex min-h-screen flex-col items-center justify-between p-24'>
-      <p className=' p-4 pt-2 '>
-
-
-        Bienvenido este es prueba del login
-      </p>
+      <div>
+        <h1>{session?.data?.user?.name}</h1>
+        <button onClick={() => signOut()}>logout</button>
+      </div>
     </main>
   );
 }
