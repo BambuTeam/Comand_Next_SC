94e4fe2f88a99f3857697e1182a8c5df4d41b3c9
nestor22 <nestor.ge222@gmail.com>
2023-10-01 21:37:36 -0600

    prettier every were

--- app/HomeTest.tsx
+++ app/HomeTest.tsx
@@ -30,7 +30,7 @@ const HomeTest = () => {
   return (
     <div className='bg-white'>
       <div className=' text-lg text-green-950'>
-        <h1 className='px-2 py-3 m-4 text-[3.5rem]'>CONTROL DE COMANDAS</h1>
+        <h1 className='m-4 px-2 py-3 text-[3.5rem]'>CONTROL DE COMANDAS</h1>
         <hr className='text-green-950' />
       </div>
 
