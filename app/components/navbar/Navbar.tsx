5ecf6038f1d8dc365bfe234ee6d10ae3ff22ba04
Bambu Team |  Luis Enrique Concuá <75746976+BambuTeam@users.noreply.github.com>
2023-10-27 19:01:18 -0600

    Navbar Custom
    Navbar Custom

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,13 +1,13 @@
 // import Link from 'next/link';
-import logo from '@/public/Images/Navbar/logo_bambu_oficial.png';
+import logo from '@/public/Images/Navbar/logoBambuWhite.svg';
 import isotipo from '@/public/Images/Navbar/bambuisotipo.svg';
 import Image from 'next/image';
 
 const Navbar: React.FC = () => {
   return (
-    <nav className=' flex h-20 items-center justify-between bg-[#0d0d0d] p-3 text-[#fefefe]'>
+    <nav className=' ... fixed left-0 right-0 top-0 z-10 flex h-20 items-center justify-between bg-gradient-to-r from-[#0d0d0d]  text-[#fefefe]'>
       <div className='ml-20 flex w-1/6 items-center'>
-        <Image src={logo} height={10} width={200} alt='Logo Bambu' />
+        <Image src={logo} height={50} width={150} alt='Logo Bambu' />
       </div>
       <div className='w-2/3'>
         {/* <Link href='/'>Inicio</Link>
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,29 +2,49 @@ import React from 'react';
 
 function LoginSesion() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-gray-100'>
-      <div className='w-96 rounded bg-white p-8 shadow-md'>
-        <h2 className='mb-4 text-2xl'>Iniciar sesión</h2>
-        <form>
-          <div className='mb-4'>
-            <label htmlFor='email' className='mb-2 block text-sm font-bold text-gray-700'>
-              Correo electrónico
-            </label>
-            <input type='email' id='email' className='w-full rounded-md border px-3 py-2' />
+    <div className='flex min-h-screen items-center justify-center bg-[#1a1a1a]'>
+      <div className='flex w-full max-w-screen-xl'>
+        {/* Columna Izquierda */}
+        <div className='... w flex min-h-screen items-center justify-center bg-gradient-to-r from-indigo-500 from-10% via-sky-500  via-30% to-emerald-500 to-90%'>
+          {/* Contenido de la columna izquierda */}
+          <div>
+            <h1 className='w-50 mb-4 flex items-center justify-center text-center text-6xl text-white'>
+              Aca tengo que renderizar un ptx!! slider
+            </h1>
+            {/* Agrega el contenido que desees */}
           </div>
-          <div className='mb-4'>
-            <label htmlFor='password' className='mb-2 block text-sm font-bold text-gray-700'>
-              Contraseña
-            </label>
-            <input type='password' id='password' className='w-full rounded-md border px-3 py-2' />
+        </div>
+
+        {/* Columna Derecha (Formulario de inicio de sesión) */}
+        <div className='flex w-1/2 items-center justify-center'>
+          <div className='w-96 rounded bg-white p-8 shadow-md '>
+            <h2 className='mb-4 text-2xl'>Iniciar sesión</h2>
+            <form>
+              <div className='mb-4'>
+                <label htmlFor='email' className='mb-2 block text-sm font-bold text-gray-700'>
+                  Correo electrónico
+                </label>
+                <input type='email' id='email' className='w-full rounded-md border px-3 py-2' />
+              </div>
+              <div className='mb-4'>
+                <label htmlFor='password' className='mb-2 block text-sm font-bold text-gray-700'>
+                  Contraseña
+                </label>
+                <input
+                  type='password'
+                  id='password'
+                  className='w-full rounded-md border px-3 py-2'
+                />
+              </div>
+              <button
+                type='submit'
+                className='rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none'
+              >
+                Iniciar sesión
+              </button>
+            </form>
           </div>
-          <button
-            type='submit'
-            className='rounded-md bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:outline-none'
-          >
-            Iniciar sesión
-          </button>
-        </form>
+        </div>
       </div>
     </div>
   );
